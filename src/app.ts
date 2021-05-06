32fb76d6bb9a966cbf8d8c387d07a7d63e609829
IanTorquato <iantorquato2@gmail.com>
2021-05-06 09:29:05 -0300

    Simple error handler

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,14 @@ import { routes } from './routes'
 
 const server: FastifyInstance = Fastify({ logger: true })
 
+server.setErrorHandler((error, request, reply) => {
+  console.log(error)
+
+  const statusCode = error.statusCode ? error.statusCode : error.validation ? 400 : 500
+
+  reply.status(statusCode).send({ error: error.message })
+})
+
 server.register(routes)
 
 export { server }
